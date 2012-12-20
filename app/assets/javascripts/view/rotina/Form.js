25709891fa9d450dfbd245e74d274fd6f8f75003
Christiano Milfont <cmilfont@gmail.com>
2012-12-19 21:54:17 -0300

    Aula 3 - exercicio

--- app/assets/javascripts/view/rotina/Form.js
+++ app/assets/javascripts/view/rotina/Form.js
@@ -1,3 +1,8 @@
+//1 - adicionar um hidden no form para o id
+//2 - passar um model na config do form e setar no Basic com loadRecod
+//3 - quando clicar no save, pegar o model original via getRecord
+//4 - save e quando terminar, atualizar o item na tree
+
 Ext.define('Workout.view.rotina.Form', {
   extend: 'Ext.form.Panel',
   alias: 'widget.rotinaform',
