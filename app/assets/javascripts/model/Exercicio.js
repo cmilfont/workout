6ecaf2f55f1b7141b844b2fe96ef0dd3e272d476
Christiano Milfont <cmilfont@gmail.com>
2013-01-08 18:54:14 -0300

    resetando app para curso

--- app/assets/javascripts/model/Exercicio.js
+++
@@ -1,7 +0,0 @@
-Ext.define('Workout.model.Exercicio', {
-  extend: 'Ext.data.Model',
-  fields: ['id','titulo', 'descricao'],
-  proxy: {
-    type: 'rest', format: 'json', url: '/exercicios'
-  }
-});
--- app/assets/javascripts/model/Item.js
+++
@@ -1,9 +0,0 @@
-Ext.define('Workout.model.Item', {
-  extend: 'Ext.data.Model',
-  fields: [
-    'id','repeticao', 'tempo', 'rotina_id', 'exercicio_id'
-  ],
-  belongsTo: [
-    {model: 'Workout.model.Exercicio', associationKey: 'exercicio', getterName: 'exercicio'}
-  ]
-});
