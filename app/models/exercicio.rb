6278b97296a12da2958fc48e6e0ce59dfab4bfd5
Christiano Milfont <cmilfont@gmail.com>
2013-02-16 16:47:52 -0300

    validação no exercicio

--- app/models/exercicio.rb
+++ app/models/exercicio.rb
@@ -1,3 +1,7 @@
 class Exercicio < ActiveRecord::Base
-  attr_accessible :descricao, :titulo  
+  attr_accessible :descricao, :titulo
+  
+  validates_presence_of :titulo, :descricao
+  validates_uniqueness_of :titulo  
+  
 end
