0450619e1efd5b038fd4ae7b96e9c8c322a2ac59
Christiano Milfont <cmilfont@gmail.com>
2013-02-16 23:00:48 -0300

    mudando de dev para prod

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require ext-all-dev
+//= require ext-all
 //= require base
 //= require_tree ./app/view/dashboard
 //= require ./app/view/Viewport
